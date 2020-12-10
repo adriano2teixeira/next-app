138fe30c73d6bea5d79c54c1d2b9df7b024ee85e
adriano2teixeira <adrinao.teixeira@goncaz.ao>
2020-12-10 20:00:55 +0100

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import { Link } from "next/link";
-
 export default function App() {
   return (
     <>
       <h1>Home page</h1>
       <br />
-      <Link>
-        <a href="/contact">Contacte</a>
-      </Link>
+
+      <a href="/contact">Contacte</a>
     </>
   );
 }
